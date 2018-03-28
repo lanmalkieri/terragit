63f1bc5964e7c565204947e6c4919e2c8e695ddb
Stobiewankenobi <cstobie@veritone.com>
2018-03-27 18:12:27 -0700

    initial commit

---
+++ lambda-git-terraform.py
@@ -0,0 +1,102 @@
+#---------------------------------------------------#
+#     <Christopher Stobie> cjstobie@gmail.com       #
+#---------------------------------------------------#
+# Function designed to parse incoming git messages
+# and auto tag our commits to master based on what's
+# in the aws-blueprints/version.txt files
+#---------------------------------------------------#  
+
+import json
+import re
+import sys
+import base64
+import os
+from botocore.vendored import requests
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+def post_to_slack(name, new_ver):
+    slack_hook_url = os.environ.get('slack_hook_url')
+    slack_message = {
+        'channel': 'terraform-versions',
+        'username': "TerraGit",
+        'icon_emoji': ':github:',
+        'attachments': [
+            {
+                "fallback": "%s-%s",
+                "color": "#36a64f",
+                "title": "Terraform Git Tagger",
+                "fields": [
+                    {
+                        "title": name,
+                        "value": new_ver,
+                        "short": "false"
+                    }
+                ]
+            }
+        ]
+    }
+    print(slack_message)
+
+    req = Request(slack_hook_url, json.dumps(slack_message))
+
+    try:
+        response = urlopen(req)
+        response.read()
+        print("Message posted to %s", slack_message)
+    except HTTPError as e:
+        print("Request failed: %d %s", e.code, e.reason)
+    except URLError as e:
+        print("Server connection failed: %s", e.reason)
+    
+def parse_git_sns(data):
+    git_token = os.environ.get('git_token')
+    headers = {'Authorization': 'token %s' % git_token}
+
+    changed_files = []
+    tag_list = []
+    modules = {}
+
+    if data["ref"] != "refs/heads/master":
+        print("ref not master")
+        sys.exit(0)
+
+    for key in data["commits"]:
+        if key["distinct"] == True:
+            commit_hash = key["id"]
+
+            for obj in key["modified"]:
+                changed_files.append(obj)
+            for obj in key["added"]:
+                changed_files.append(obj)
+
+    for git_file in changed_files:
+        if "version.txt" in git_file:
+            url = "https://api.github.com/repos/%s/%s/contents/%s" % (org, repo_name, git_file)
+            file_r = requests.get(url, headers=headers)
+            contents = file_r.json()["content"]
+            new_version = base64.b64decode(contents).split('\n', 1)[0].split(":")[1].lstrip()
+            module_name = git_file.split("/")[1]
+            modules[module_name] = new_version
+
+    for name, ver in modules.iteritems():
+        new_ver = name + "-" +  ver
+        tag_json = {
+            "ref": "refs/tags/%s" % new_ver,
+            "sha": commit_hash
+        }
+        print("New tag: %s" % tag_json)
+        tag_headers = {'Authorization': 'token %s' % git_token, 'Content-Type': 'application/json'}
+        tag_url = "https://api.github.com/repos/%s/%s/git/refs" % (org, repo_name)
+        tag_r = requests.post(tag_url, headers=tag_headers, json=tag_json)
+        print("Tag Response: %s" % tag_r.text)
+
+        if "sha" not in tag_r.text:
+            print("No sha found in tag response")
+            sys.exit(0)
+        else:
+            post_to_slack(name, new_ver)
+
+def lambda_handler(event, context):
+    message = event['Records'][0]['Sns']['Message']
+    print("From SNS: " + message)
+    parse_git_sns(json.loads(message))
